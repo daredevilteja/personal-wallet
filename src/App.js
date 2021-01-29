079daeab208103e0eb5b187cc017da71f682bbfb
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-01-30 00:00:44 +0530

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Personal Wallet UI</h1>
+      </div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">All Wallets</Link>
+            </li>
+            <li>
+              <Link to="/newWallet">New Wallet</Link>
+            </li>
+            <li>
+              <Link to="/checkBalance">Check Balance</Link>
+            </li>
+            <li>
+              <Link to="/addFunds">Add Funds</Link>
+            </li>
+            <li>
+              <Link to="/spendFunds">Spend Funds</Link>
+            </li>
+            <li>
+              <Link to="/allTransactions">All Transactions</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <Switch>
+          <Route path="/newWallet">New Wallet</Route>
+          <Route path="/checkBalance">s</Route>
+          <Route path="/addFunds">t</Route>
+          <Route path="/spendFunds">td</Route>
+          <Route path="/allTransactions">db</Route>
+          <Route path="/">d</Route>
+        </Switch>
+      </div>
     </div>
   );
 }
