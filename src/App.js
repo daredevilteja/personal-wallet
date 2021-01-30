0202496528a9705c09545042e136ee8fed4563ce
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-01-30 12:31:54 +0530

    Added new components

--- src/App.js
+++ src/App.js
@@ -5,42 +5,56 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className="Title">
         <h1>Personal Wallet UI</h1>
       </div>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">All Wallets</Link>
-            </li>
-            <li>
-              <Link to="/newWallet">New Wallet</Link>
-            </li>
-            <li>
-              <Link to="/checkBalance">Check Balance</Link>
-            </li>
-            <li>
-              <Link to="/addFunds">Add Funds</Link>
-            </li>
-            <li>
-              <Link to="/spendFunds">Spend Funds</Link>
-            </li>
-            <li>
-              <Link to="/allTransactions">All Transactions</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div>
-        <Switch>
-          <Route path="/newWallet">New Wallet</Route>
-          <Route path="/checkBalance">s</Route>
-          <Route path="/addFunds">t</Route>
-          <Route path="/spendFunds">td</Route>
-          <Route path="/allTransactions">db</Route>
-          <Route path="/">d</Route>
-        </Switch>
+      <div className="body-container">
+        <div className="navbar">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">All Wallets</Link>
+              </li>
+              <li>
+                <Link to="/newWallet">New Wallet</Link>
+              </li>
+              <li>
+                <Link to="/checkBalance">Check Balance</Link>
+              </li>
+              <li>
+                <Link to="/addFunds">Add Funds</Link>
+              </li>
+              <li>
+                <Link to="/spendFunds">Spend Funds</Link>
+              </li>
+              <li>
+                <Link to="/allTransactions">All Transactions</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="main-container">
+          <Switch>
+            <Route path="/newWallet">
+              <NewWallet />
+            </Route>
+            <Route path="/checkBalance">
+              <CheckBalance />
+            </Route>
+            <Route path="/addFunds">
+              <AddFunds />
+            </Route>
+            <Route path="/spendFunds">
+              <SpendFunds />
+            </Route>
+            <Route path="/allTransactions">
+              <AllTransactions />
+            </Route>
+            <Route path="/">
+              <AllWallets />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </div>
   );
