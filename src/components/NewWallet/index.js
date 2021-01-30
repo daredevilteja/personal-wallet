c842d9cae3655ba140abcf9c3b246583c7564db2
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-01-30 14:58:01 +0530

    Updated NewWallet

--- src/components/NewWallet/index.js
+++ src/components/NewWallet/index.js
@@ -1,6 +1,22 @@
-import React from 'react'
-import './styles.css'
+import React from "react";
+import "./styles.css";
 
 export default function NewWallet() {
-    return <></>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" /> <br />
+        <label htmlFor="phone">Phone</label>
+        <input type="number" id="phone" name="Phone" /> <br />
+        <label htmlFor="amount">Amount (RS)</label>
+        <input type="text" id="amount" name="amount" /> <br />
+        <input type="submit" value="Submit" /> <br />
+      </form>
+    </>
+  );
 }
